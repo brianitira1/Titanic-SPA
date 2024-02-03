5cd7ab89f085852e8a1da52dc67812ec7313276e
Brian-Itira <136370929+Brian-Itira@users.noreply.github.com>
2024-02-03 22:54:01 +0000

    working with animation in the services page

--- src/components/ServicesPage.jsx
+++ src/components/ServicesPage.jsx
@@ -1,26 +1,33 @@
 import React from "react";
-
+import { motion } from "framer-motion";
 import NavBar from "../components/NavBar";
-import heroImage from "../assets/images/heroimage.jpg";
-
+import servicesimage from "../assets/images/servicesimage.jpg";
 import "../styles/ServicesPage.css";
 
 const ServicesPage = () => {
   return (
-
-
     <>
-
       <NavBar />
-
-
-      <div className="hero-container " id="services-container">
-        <h1>Services Page</h1>
-        <img src={heroImage} alt="Hero Image" className="img-fluid hero-image " />
-      <div className="overlay-services"></div> 
-        
+      <div className="hero-container" id="services-container">
+        <motion.h1
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1 }}
+          className="text-center text-white mt-5 mb-3"
+          id="services-text"
+        >
+          Discover Our Luxurious Services
+        </motion.h1>
+        <motion.img
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 1 }}
+          src={servicesimage}
+          alt="Hero Image"
+          className="img-fluid hero-image"
+        />
+        <div className="overlay-services"></div>
       </div>
-
     </>
   );
 };
