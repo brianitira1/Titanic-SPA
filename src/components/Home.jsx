bd67c593ec9ca16b8f361489a663ee36a0f95e8c
soloKomrade <136370929+Brian-Itira@users.noreply.github.com>
2024-02-04 22:25:05 +0000

    fix: Add styles to Hero component, add styles to NavBar component, add styles to CustomForm component, remove unnecessary div elements and remove scroll links

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,37 +1,28 @@
-import React from "react";
 import NavBar from "../components/NavBar";
 import Hero from "../components/Hero";
 import Footer from "../components/Footer";
 import AboutSection from "../sections/AboutSection";
-import { Link } from "react-scroll;
+
+import "../styles/Hero.css";
+import "../styles/NavBar.css";
+import "../styles/CustomForm.css";
 
 const Home = () => {
   return (
     <>
       <NavBar />
-      <Hero />
-      <AboutSection />
-      <Footer />
-
-      <div className="scroll-links">
-        <Link
-          to="hero-section"
-          spy={true}
-          smooth={true}
-          offset={-70}
-          duration={500}
-        >
-          Scroll to Hero
-        </Link>
-        <Link
-          to="about-section"
-          spy={true}
-          smooth={true}
-          offset={-70}
-          duration={500}
-        >
-          Scroll to About
-        </Link>
+      <div>
+        <div>
+          <div>
+            <Hero />
+          </div>
+          <div>
+            <AboutSection />
+          </div>
+          <div>
+            <Footer />
+          </div>
+        </div>
       </div>
     </>
   );
