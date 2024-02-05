3b4d9c51f59811f847b031a1b18fba83d1e128db
soloKomrade <136370929+Brian-Itira@users.noreply.github.com>
2024-02-05 00:13:22 +0000

    feat: Add hero image to Beauty component, update background color and max width and update styles

--- src/components/Beauty.jsx
+++ src/components/Beauty.jsx
@@ -2,14 +2,21 @@ import React from "react";
 import NavBar from "../components/NavBar";
 import "../styles/BeautyPage.css";
 
+import heroImage from "../assets/images/heroimage.jpg";
+
 import ClientAppointmentPage from "../utils/ClientAppointmentPage";
 
 const Beauty = () => {
   return (
     <>
       <NavBar />
-      <div className="container" id="beauty-container">
-        <h1>Beauty</h1>
+      <div className=" hero-container " id="beauty-container">
+        <img
+          src={heroImage}
+          alt="Hero Image"
+          className="img-fluid hero-image"
+        />
+        <div className="overlay-form"></div>
         <ClientAppointmentPage />
       </div>
     </>
